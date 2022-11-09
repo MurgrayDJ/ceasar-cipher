c4f46e8993144482c84ebd501488a30ab0c3d51a
murgraydj <murgraydj@gmail.com>
2022-11-09 15:14:51 -0500

    Adds is_upper? and is_lower? to String class
    These methods check if a letter is upper or lower case.

--- cipher.rb
+++ cipher.rb
@@ -2,6 +2,16 @@
 # Date created: 11/9/2022 
 # Purpose: Simulates a ceasar cipher, where user can input the string and the shift value.
 
+class String
+    def is_upper?
+      self == self.upcase
+    end
+  
+    def is_lower?
+      self == self.downcase
+    end
+end
+
 print "Text to cipher: "
 orig_string = gets
 
