c24780bb759907a5e0eacabcf2b4369d9f2abe20
murgraydj <murgraydj@gmail.com>
2022-11-09 16:52:46 -0500

    Removes String class methods
    Also moves some functionality out of the ciper method and into a new
    method

--- cipher.rb
+++ cipher.rb
@@ -2,16 +2,6 @@
 # Date created: 11/9/2022 
 # Purpose: Simulates a ceasar cipher, where user can input the string and the shift value.
 
-class String
-    def is_upper?
-      self == self.upcase
-    end
-  
-    def is_lower?
-      self == self.downcase
-    end
-end
-
 def get_text
     print "Text to cipher: "
     orig_string = gets
@@ -24,27 +14,27 @@ def get_shift_num(orig_string)
     cipher(orig_string, shift_num)
 end
 
+def convert_char(ltr_array, current_char, shift_num)
+    char_index = ltr_array.find_index(current_char)
+    ltr_array[(char_index + shift_num) % ltr_array.length]
+end
+
 def cipher(orig_string, shift_num)
     uppers = ("A".."Z").to_a
     lowers = ("a".."z").to_a
     new_string = ''
 
     orig_string.each_char do |current_char|
-        if uppers.include?(current_char.upcase)
-            if current_char.is_upper?
-                char_index = uppers.find_index(current_char)
-                new_string << uppers[(char_index + shift_num) % uppers.length]
-            else
-                char_index = lowers.find_index(current_char)
-                new_string << lowers[(char_index + shift_num) % lowers.length]
-            end
+        if uppers.include?(current_char)
+            new_string << convert_char(uppers, current_char, shift_num)
+        elsif lowers.include?(current_char)
+            new_string << convert_char(lowers, current_char, shift_num)
         else
             new_string << current_char
         end
     end
     puts new_string
 end
-
 
 def run_program
     get_text
