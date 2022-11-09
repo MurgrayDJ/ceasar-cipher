b8f6cca92c24f3b9bc1d212e7f3651038b2b4c20
murgraydj <murgraydj@gmail.com>
2022-11-09 14:59:35 -0500

    Takes in cipher text and shift number from user

--- cipher.rb
+++ cipher.rb
@@ -1,3 +1,9 @@
 # Author: Murgray D. John
 # Date created: 11/9/2022 
 # Purpose: Simulates a ceasar cipher, where user can input the string and the shift value.
+
+print "Text to cipher: "
+orig_string = gets
+
+print "Letters to shift: "
+shift_num = gets
