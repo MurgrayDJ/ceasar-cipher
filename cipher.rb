dde43379561ed7a2d386eccbb4b8d1db6727307d
murgraydj <murgraydj@gmail.com>
2022-11-09 16:32:29 -0500

    Adds functional cipher method
    This method does the cipher with the inputed string and shift number,
    and prints out the result.

--- cipher.rb
+++ cipher.rb
@@ -15,16 +15,39 @@ end
 def get_text
     print "Text to cipher: "
     orig_string = gets
+    get_shift_num(orig_string)
 end
 
-def get_shift_num
+def get_shift_num(orig_string)
     print "Shift number: "
-    shift_num = gets
+    shift_num = gets.to_i
+    cipher(orig_string, shift_num)
 end
+
+def cipher(orig_string, shift_num)
+    uppers = ("A".."Z").to_a
+    lowers = ("a".."z").to_a
+    new_string = ''
+
+    orig_string.each_char do |current_char|
+        if uppers.include?(current_char.upcase)
+            if current_char.is_upper?
+                char_index = uppers.find_index(current_char)
+                new_string << uppers[(char_index + shift_num) % uppers.length]
+            else
+                char_index = lowers.find_index(current_char)
+                new_string << lowers[(char_index + shift_num) % lowers.length]
+            end
+        else
+            new_string << current_char
+        end
+    end
+    puts new_string
+end
+
 
 def run_program
     get_text
-    get_shift_num
 end
 
 run_program
