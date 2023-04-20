22e03229c738f562068462575a984acc58a74bfc
murgraydj <murgraydj@gmail.com>
2023-04-20 19:02:26 -0400

    Surrounds Cipher test code with Cipher class

--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -1,8 +1,11 @@
 require './cipher.rb'
 
-describe "#cipher" do
-  it "shifts numbers over by 1" do
-    cipher = Cipher.new
-    expect(cipher.cipher("hello", 1)[0]).to eql("ifmmp")
+describe Cipher do
+  subject{Cipher.new}
+  
+  describe "#cipher" do
+    it "shifts numbers over by 1" do
+      expect(subject.cipher("hello", 1)[0]).to eql("ifmmp")
+    end
   end
 end
